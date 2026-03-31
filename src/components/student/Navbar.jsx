c66ace37050850ceeaec29a928f571bfcaa41ac1
MarcusJadhav <marcusjadhav@gmail.com>
2026-03-31 19:26:20 +0530

    Navbar created

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useClerk, UserButton, useUser } from '@clerk/react'
 
 const Navbar = () => {
+
+  const isCourseListPage = location.pathname.includes('/course-list')
+
+  const {openSignIn} = useClerk()
+  const {user} = useUser()
+
   return (
-    <div>
-    <h1>Navbar</h1>    
+    <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4
+     ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
+
+      <img src="./src/assets/logo.png" alt="logo" className='w-10 lg:w-32 sm:w-20 cursor-pointer' />
+
+      <div className='hidden md:flex items-center gap-5 text-gray-500'>
+        <div className='flex items-center gap-5'>
+          
+          { user && <>
+          <button>
+            Become Educator
+            </button> |
+          <Link to='/my-enrollments'> My Enrollments
+          </Link>
+          </> 
+          }
+
+        </div>
+        
+        {user ? <UserButton /> :
+        <button onClick={()=> openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full hover:cursor-pointer'>
+          Create Account
+          </button>
+        }
+
+      </div>
+
+      {/* Mobile Screen */}
+      <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+        <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
+           { user && 
+           <>
+          <button>
+            Become Educator
+            </button> |
+          <Link to='/my-enrollments'> My Enrollments
+          </Link>
+          </> 
+          }
+        </div>
+        {user ? <UserButton /> :
+        <button onClick={()=> openSignIn()}>
+          <img src="./src/assets/user-icon.png" alt="user-icon" className='w-8 hover:cursor-pointer'>
+          </img>
+          </button>
+          }
+      </div>
     </div>
   )
 }
