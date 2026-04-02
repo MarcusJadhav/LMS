4226d33bd51934c08802effb68c6b580276b0f07
MarcusJadhav <marcusjadhav@gmail.com>
2026-04-02 15:43:10 +0530

    Search bar implemented

--- src/components/student/Hero.jsx
+++ src/components/student/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from './../../assets/assets'
+import SearchBar from './../../components/student/SearchBar'
 
 const Hero = () => {
   return (
@@ -10,7 +11,10 @@ const Hero = () => {
     <p className='md:block hidden text-gray-500 max-w-2xl mx-auto'>We bring together world-class instructors, interactive content, and a supportive community to help you achieve your personal and professional goals.</p>
 
     <p className='md:hidden text-gray-500 max-w-sm mx-auto'>We bring together world-class instructors, interactive content, and a supportive community to help you achieve your personal and professional goals.</p>
+
+    <SearchBar />
     </div>
+
   )
 }
 
--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
        <Hero />
+      
     </div>
   )
 }
